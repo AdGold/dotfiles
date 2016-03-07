ac40b5377dd4aad24be68040ce3337996e6cb5c7
Adrian Goldwaser <agold@hotmail.com>
2016-03-08 02:41:15 +1100

    Add symlink maker

---
+++ makesymlinks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dotdir=~/dotfiles
+olddir=~/dotfiles_old
+dotfiles="vimrc zshrc Xresources gitconfig pythonstartup.py vimrc xinitrc xmodmap i3/config i3/i3status config/touchegg/touchegg.conf ssh/config"
+
+mkdir -p $olddir
+cd $dir
+
+dotfolders="ssh i3 config/touchegg"
+for folder in $dotfolders; do
+    mkdir -p ~/.$folder
+done
+
+echo "Moving any existing dotfiles from ~ to $olddir"
+
+for file in $dotfiles; do
+    mv ~/.$file ~/$olddir/
+    rm ~/.$file
+    echo "Creating symlink to .$file in home directory."
+    ln -s $dotdir/$file ~/.$file
+done
+
+echo "done"
