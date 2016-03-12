ce9e19cf1b90cb27c313828ee1fd57d708bb7440
Adrian Goldwaser <agold@hotmail.com>
2016-03-13 01:00:09 +1100

    Switch to polling because inotifywait doesn't work

--- screenlayout/select-display.sh
+++ screenlayout/select-display.sh
@@ -1,12 +1,20 @@
 external=/sys/class/drm/card0-HDMI-A-1/status
 
-while inotifywait -e modify,create,delete,open,close,close_write,access $external
-do
-    if [ `cat $external` = 'disconnected' ]
-    then
-        ~/.screenlayout/one-screen.sh
-    else
-        ~/.screenlayout/both-screens.sh
-    fi
+prev=""
 
+# while inotifywait -e modify,create,delete,open,close,close_write,access $external
+while true
+do
+    status="`cat $external`"
+    if [ "$status" != "$prev" ]
+    then
+        if [ $status = 'disconnected' ]
+        then
+            ~/.screenlayout/one-screen.sh
+        else
+            ~/.screenlayout/both-screens.sh
+        fi
+        prev=$status
+    fi
+    sleep 5
 done
