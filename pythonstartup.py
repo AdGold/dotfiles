4400484f12ad25309acf30bb73eda07532e403dc
Adrian Goldwaser <ad.goldwaser@gmail.com>
2020-05-10 01:08:08 +1000

    Fix python startup

--- pythonstartup.py
+++ pythonstartup.py
@@ -3,28 +3,34 @@ import itertools, collections
 
 readline.parse_and_bind("C-spc: complete")
 
-h = [None]
+history = [None]
+
 
 class Prompt:
     def __init__(self, str='h[%d] >>> '):
-        self.str = str;
+        self.str = str
 
     def __str__(self):
         try:
-            if _ not in [h[-1], None, h]: h.append(copy.deepcopy(_));
+            if _ not in [history[-1], None, history]:
+                history.append(copy.deepcopy(_))
         except NameError:
             pass
         except TypeError:  # weird object.__new__(self) not safe error
             pass
-        return self.str % len(h);
+        try:
+            return self.str % len(history)
+        except TypeError:  # history was overwritten)
+            return 'h[X] >>> '
+        except Exception as ex:
+            return 'EXCEPTION(%s)' % ex
 
     def __radd__(self, other):
         return str(other) + str(self)
 
 
-if os.environ.get('TERM') in [ 'xterm', 'vt100', 'rxvt-unicode']:
+if os.environ.get('TERM') in ['xterm', 'vt100', 'rxvt-unicode']:
     sys.ps1 = Prompt('\001\033[0:1;31m\002h[%d] >>> \001\033[0m\002')
 else:
     sys.ps1 = Prompt()
 sys.ps2 = ''
-
