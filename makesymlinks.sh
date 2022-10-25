938c1abc74e9aa7d839a119e017266563dbac936
Adrian Goldwaser <ad.goldwaser@gmail.com>
2022-10-25 15:49:40 +0100

    Remove old dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,15 +5,12 @@ olddir=~/.git_installs/dotfiles_old
 dotfiles="
 vimrc
 zshrc
-Xresources
 gitconfig
 gitignore
 pythonstartup.py
-xinitrc
 config/ranger/rc.conf
 config/ranger/colorschemes/dracula.py
 config/zathura/zathurarc
-config/redshift.conf
 config/kitty/dracula.conf
 config/kitty/kitty.conf
 tmux.conf
@@ -22,18 +19,12 @@ config/tmux/tmuxline.conf
 config/kitty/dracula.conf
 config/kitty/kitty.conf
 ssh/config
-gtkrc-2.0
-urxvt/ext/tabbedex
-urxvt/ext/clipboard
-urxvt/ext/font-size
-vim/.ycm_extra_conf.py
 scripts/makesymlinks
 "
 
-
 cd $dir
 
-dotfolders="ptpython ssh i3 config/touchegg config/ranger/colorschemes config/zathura config/tmux screenlayout urxvt/ext scripts"
+dotfolders="ssh config/ranger/colorschemes config/zathura config/tmux scripts"
 for folder in $dotfolders; do
     mkdir -p ~/.$folder
 done
@@ -46,7 +37,4 @@ for file in $dotfiles; do
     echo "Creating symlink to .$file in home directory."
     ln -s $dotdir/$file ~/.$file
 done
-
-xrdb -merge ~/.Xresources
-
 echo "done"
